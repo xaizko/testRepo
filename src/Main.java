0abf8d282f6d6ea08dbaea89b46896c8a5dea831
BT_1N3_14 <sample@bths.edu>
2023-12-15 09:03:25 -0500

    v1.03

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("blehhhhhhhhhhhhhh");
-        System.out.println("What the scallop");
+        System.out.println("Dear neptune");
         System.out.println("hi justin");
         System.out.println("Hey Tony");
         System.out.println("99% of Gamblers quit before they hit big, don't stop gambling ");
