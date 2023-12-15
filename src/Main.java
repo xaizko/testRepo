c531e17b598bfa9cd869ed3cb93d7f464aab5664
BT_1N3_13 <bths@sample.edu>
2023-12-15 08:56:30 -0500

    added even checker

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("blehhhhhhhhhhhhhh");
         System.out.println("What the scallop");
+        System.out.println("hi justin");
     }
 }
---
+++ src/Math.java
@@ -0,0 +1,7 @@
+public class Math {
+    private Math(){}
+
+    public static boolean even(int num) {
+        return (num % 2 == 0);
+    }
+}
