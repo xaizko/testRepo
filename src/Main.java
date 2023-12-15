11bc28dc93e5804fec30151cee18b0db85d630aa
BT_1N3_14 <sample@bths.edu>
2023-12-15 08:59:16 -0500

    v1.02

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("blehhhhhhhhhhhhhh");
         System.out.println("What the scallop");
         System.out.println("hi justin");
+        System.out.println("Hey Tony");
+        System.out.println("99% of Gamblers quit before they hit big, don't stop gambling ");
     }
 }
